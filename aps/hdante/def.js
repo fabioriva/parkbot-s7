ffb55881ac3874f00aadb5fb3ea8d9f327817755
Fabio Riva <f.riva@sotefin.ch>
2022-10-10 09:44:45 +0200

    update package.json

--- aps/hdante/def.js
+++ aps/hdante/def.js
@@ -13,7 +13,7 @@ const ALARM_LEN = 8
 exports.ALARM_LEN = ALARM_LEN
 exports.DB_ALARM_INIT = 6
 exports.DB_ALARM_LEN = 64 * ALARM_LEN
-exports.DBS_ALARM = [] // 531, 532] // EU1, EU2, EL
+exports.DBS_ALARM = [] // 531, 532, 533] // EU1, EU2, EL
 
 const CARDS = 52
 const CARD_LEN = 10
