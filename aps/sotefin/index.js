370a95b1a18c7b4e8f4f6fdfffd3aaa952418572
Fabio Riva <f.riva@sotefin.ch>
2022-10-11 15:35:11 +0200

    add test with local plc

---
+++ aps/sotefin/index.js
@@ -0,0 +1,24 @@
+const logger = require('pino')()
+const snap7 = require('bindings')('node_snap7_client')
+
+async function forever (client) {
+  try {
+    const buffer = await client.ReadArea(0x84, 505, 0, 142, 0x02)
+    logger.info(buffer)
+  } catch (e) {
+    logger.error(e)
+  }
+}
+
+async function main () {
+  try {
+    const client = new snap7.Snap7Client()
+    await client.ConnectTo('192.168.20.55', 0, 1)
+    logger.info('PLC %s is online', '192.168.20.55')
+    setInterval(() => forever(client), 500)
+  } catch (e) {
+    logger.error(e)
+  }
+}
+
+main()
